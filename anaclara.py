530e3b98b588bd3cfb106d7f6d27cf6071f788b0
ana27clara06 <4n4clara@gmail.com>
2023-10-18 11:30:59 -0300

    first commit

---
+++ anaclara.py
@@ -0,0 +1,7 @@
+n1 = int (input("digite o primeiro numero inteiro"))
+n2 = int (input("digite o segundo numero inteiro"))
+if n1 >= n2:
+    print (f"o primeiro numero: {n1} é maior ou igual a {n2}")
+else:
+    print(f"o primeiro numero; {n1} é menor que {n2}")
+    
